e97ea7d5b59ea4c994bf389650035a7c366be6b7
Denis C de Azevedo <denis.azevedo@gmail.com>
2018-10-14 15:39:02 -0300

    refactor: change contact form

---
+++ content/obrigado.md
@@ -0,0 +1,11 @@
+---
+# title: "Obrigado pelo contato"
+title: "Contato"
+# type: "Page"
+---
+
+<!-- FIXME Definir págiana de obrigado -->
+
+Obrigado pelo contato!
+
+Voltar para a página inicial
--- static/js/custom.js
+++ static/js/custom.js
@@ -2,19 +2,35 @@
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "animationsSlider" }] */
 
 $(function () {
-  slidersCustom()
+  slidersCustom();
 })
 
 /* sliders */
-function slidersCustom () {
+function slidersCustom() {
   if ($('.owl-carousel').length) {
     // Produtos
     $(".testimonials").data('owlCarousel').reinit({
-      items: 2
+      items: 3
     });
     // Sobre
-    $(".customers").data('owlCarousel').reinit({
-      items: 1
-    });
+    // $(".customers").data('owlCarousel').reinit({ items: 1 });
   }
+}
+
+/* contact */
+function contactForm() {
+  var form = $('.contact-form');
+
+  // FIXME Dev
+  // $('#name').val('Denis');
+  // $('#email').val('john@smith.com');
+  // $('#phone').val('(48) 99176-0460');
+  // $('#subject').val('Elogio');
+  // $('#message').val('Que bonito este site!');
+
+  // Does not use ajax for form submission
+  form.submit(function() {
+    $this = $(this);
+    $.post($(this).attr('action'), $this.serialize());
+  })
 }
